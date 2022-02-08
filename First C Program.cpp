a17d0e9220d0f4dbb30b6b2da1f476c42beddf8c
nudeboy69 <98754395+nudeboy69@users.noreply.github.com>
2022-02-08 21:13:11 +0100

    Update First C Program.cpp

--- First C Program.cpp
+++ First C Program.cpp
@@ -1,37 +1,9 @@
-#include <stdio.h>
-#include <math.h>
-#include <windows.h>
-#include "First C Program.h"
+#include "Includes.h"
 
 int main(void)
 {
+	regn1();
+	float svarAfR2 = regn2();
 
-	int a;
-	int b;
-	int c;
-	int d;
-	int e;
-
-	printf( "Velkommen til halvdelen af (a*b+c/d PROCENT e) udregneren\n\n\n" );
-
-	printf("Hvad skal a vaere?\n"); //første prompt for at finde int a
-	scanf_s("%d", &a);
-
-	printf("Hvad skal b vaere?\n");
-	scanf_s("%d", &b);
-
-	printf("Hvad skal c vaere?\n");
-	scanf_s("%d", &c);
-
-	printf("Hvad skal d vaere?\n");
-	scanf_s("%d", &d);
-
-	printf("Hvad skal e vaere?\n");
-	scanf_s("%d", &e);
-
-	int svar; //ny int til svar
-	svar = (a * b + c / d % e) / 2; //hvordan svar skal udregnes
-
-	printf("Svaret er %d\n", svar); //print af svaret til console
-
+	printf("Svaret af regn2 er %f", svarAfR2);
 }
